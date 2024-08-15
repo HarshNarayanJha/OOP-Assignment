333c47efe5aa6fb89d324a0bb2042a2c173967d0
Harsh Narayan Jha <50262541+HarshNarayanJha@users.noreply.github.com>
2024-08-15 19:06:33 +0530

    q3 done

---
+++ Assignment-1/q3.java
@@ -0,0 +1,30 @@
+// 3. Write a Java Program to find the reverse of a number (Take user input).
+
+import java.util.Scanner;
+import java.lang.Math;
+
+class q2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        int rev = 0;
+
+        int t = num;
+        int d = 0;
+        while (t > 0) {
+            t /= 10;
+            d++;
+        }
+        t = num;
+        int i = d;
+        while (t > 0) {
+            rev += (t % 10) * Math.pow(10, i - 1);
+            t /= 10;
+            i--;
+        }
+
+        System.out.println(rev);
+    }
+}
