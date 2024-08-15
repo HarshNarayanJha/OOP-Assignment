680dd52421e417292aeea18e2e8e7ef0635171fd
Harsh Narayan Jha <50262541+HarshNarayanJha@users.noreply.github.com>
2024-08-15 19:09:16 +0530

    q4 done

---
+++ Assignment-1/q4.java
@@ -0,0 +1,24 @@
+// 4. WAJP to compute and display the count of occurrence of 4 in a number. E.g. 4564 will compute 2
+
+import java.util.Scanner;
+import java.lang.Math;
+
+class q2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        int t = num;
+        int c = 0;
+        while (t > 0) {
+            int d = t % 10;
+            if (d == 4) {
+                c++;
+            }
+            t /= 10;
+        }
+
+        System.out.println("4 appears " + c + " times");
+    }
+}
